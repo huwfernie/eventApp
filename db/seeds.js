0000e0e5f4c5f2fa8584f6d0cd66a0f7f001ebef
Huw Fernie <HuwFernie@gmail.com>
2017-06-29 14:28:52 +0100

    changed seeds file and schema to work with urls in images

--- db/seeds.js
+++ db/seeds.js
@@ -16,18 +16,25 @@ const eventData = [{
   image: 'http://cdn4.sportngin.com/attachments/text_block/3526/0385/B4B_Regulars_1__medium.jpg'
 },{
   name: 'Sailing on the pond',
-  category: 'sport',
+  category: 'sports - watersports - sailing',
   location: {name: 'Stoke Newington West Reservoir', lat: 51.566686, lon: -0.091324},
   website: 'http://www.northlondonsailing.org/',
   details: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-  image: './src/images/seeds/nlsa.jpg'
+  image: 'http://www.northlondonsailing.org/_/rsrc/1484999025446/home/frontmain.jpg?height=300&width=400'
 },{
   name: 'Climbing at the Castle',
-  category: 'sport',
+  category: 'sports - climbings',
   location: {name: 'The Castle climbing center', lat: 51.565245, lon: -0.092536},
   website: 'https://www.castle-climbing.co.uk/',
   details: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-  image: 'https://www.google.co.uk/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=0ahUKEwiHg_KD8eLUAhWOYVAKHZlbDXsQjRwIBw&url=https%3A%2F%2Fwww.castle-climbing.co.uk%2FEvents&psig=AFQjCNGRSlPxXYcvH_7xqACnJbqvMn-qYQ&ust=1498819803403032'
+  image: 'https://www.castle-climbing.co.uk/sites/default/files/Castle%20Outside.jpg'
+},{
+  name: 'One in Falmouth',
+  category: 'sports - watersports - sailing',
+  location: {name: 'Falmouth', lat: 50.1526, lon: -5.0663},
+  website: 'http://www.northlondonsailing.org/',
+  details: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  image: ''
 }];
 
 Event
--- models/event.js
+++ models/event.js
@@ -11,6 +11,7 @@ const eventSchema = new mongoose.Schema({
     lon: { type: String, required: true }
   },
   website: { type: String, required: false },
+  details: { type: String, required: false },
   image: { type: String, required: true }
 });
 
@@ -25,19 +26,21 @@ eventSchema
   .virtual('imageSRC')
   .get(function getImageSRC() {
     if(!this.image) return null;
+    if(this.image.match(/^http/)) return this.image;
     return `https://s3-eu-west-1.amazonaws.com/wdi-25-london-project2/${this.image}`;
   });
+
 
 eventSchema.pre('remove', function removeImage(next) {
   if(this.image) return s3.deleteObject({ Key: this.image }, next);
   next();
 });
 
-// eventSchema.pre('save', function checkPreviousImage(next) {
-//   if(this.isModified('image') && this._image) {
-//     return s3.deleteObject({ Key: this._image }, next);
-//   }
-//   next();
-// });
+eventSchema.pre('save', function checkPreviousImage(next) {
+  if(this.isModified('image') && this._image) {
+    return s3.deleteObject({ Key: this._image }, next);
+  }
+  next();
+});
 
 module.exports = mongoose.model('Event', eventSchema);
