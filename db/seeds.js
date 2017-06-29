fdf008fe7f3d2594f425ece5fda008ec62758f15
Huw Fernie <HuwFernie@gmail.com>
2017-06-29 13:41:04 +0100

    can't make the seeds upload an image to AWS

--- db/seeds.js
+++ db/seeds.js
@@ -12,18 +12,21 @@ const eventData = [{
   category: 'sport',
   location: {name: 'Finsbury Park', lat: 51.574608, lon: -0.099564},
   website: 'http://www.londonmets.org/page/show/1229784-baseball-for-beginners',
+  details: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   image: 'http://cdn4.sportngin.com/attachments/text_block/3526/0385/B4B_Regulars_1__medium.jpg'
 },{
   name: 'Sailing on the pond',
   category: 'sport',
   location: {name: 'Stoke Newington West Reservoir', lat: 51.566686, lon: -0.091324},
   website: 'http://www.northlondonsailing.org/',
+  details: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   image: './src/images/seeds/nlsa.jpg'
 },{
   name: 'Climbing at the Castle',
   category: 'sport',
   location: {name: 'The Castle climbing center', lat: 51.565245, lon: -0.092536},
   website: 'https://www.castle-climbing.co.uk/',
+  details: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   image: 'https://www.google.co.uk/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=0ahUKEwiHg_KD8eLUAhWOYVAKHZlbDXsQjRwIBw&url=https%3A%2F%2Fwww.castle-climbing.co.uk%2FEvents&psig=AFQjCNGRSlPxXYcvH_7xqACnJbqvMn-qYQ&ust=1498819803403032'
 }];
 
