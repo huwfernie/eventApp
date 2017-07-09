165fc7760ebe3321e726059228baf06a67911af9
Huw Fernie <HuwFernie@gmail.com>
2017-07-09 08:15:58 +0100

    tabbed search results working, placeholder img for map, tidied up scss again

--- src/js/controllers/events.js
+++ src/js/controllers/events.js
@@ -48,25 +48,34 @@ function EventsShowCtrl(Event, $stateParams, $state) {
 EventsSearchCtrl.$inject = ['Event', '$stateParams', '$state', '$http'];
 function EventsSearchCtrl(Event, $stateParams, $state, $http) {
   const vm = this;
+
+  // for inital manualSearch() setup
   vm.lat = 51.566686;
   vm.long = -0.091324;
   vm.limit = 10;
   vm.distance = 500;
   vm.search_results = null;
 
+  // for content Tabs:
+  vm.activeTab = null;
+  changeTab(1);
+
   function getLocation(options) {
+    // used by nearMeNow
     return new Promise(function (resolve, reject) {
       navigator.geolocation.getCurrentPosition(resolve, reject, options);
     });
   }
 
   function useData(data){
+    // used by nearMeNow
     console.log('data');
     vm.long = data.coords.longitude;
     return vm.lat = data.coords.latitude;
   }
 
   function nearMeNow() {
+    // use HTML 5 geo location, then call manualSearch with location result
     getLocation()
     .then((position) => {
       console.log(position);
@@ -79,9 +88,8 @@ function EventsSearchCtrl(Event, $stateParams, $state, $http) {
   }
   vm.nearMeNow = nearMeNow;
 
-
-
   function manualSearch() {
+    // search the database with manual inputs
     console.log('manualSearch');
     $http({
       url: 'http://localhost:7000/api/eventsSearch',
@@ -95,10 +103,29 @@ function EventsSearchCtrl(Event, $stateParams, $state, $http) {
   vm.manualSearch = manualSearch;
 
   function clearResults() {
+    // for clearing search results from display
     console.log('clearResults');
     vm.search_results = null;
   }
   vm.clearResults = clearResults;
+
+
+  function changeTab(x) {
+    // for changing Tab in tabbed browsing
+    const allTabs = document.getElementsByClassName('tabs');
+    const allContent = document.getElementsByClassName('tab_content');
+    if(vm.activeTab !== x) {
+      vm.activeTab = x;
+      for(let i = 0; i < allTabs.length; i++) {
+        allTabs[i].classList.toggle('active');
+      }
+      for(let i = 0; i < allContent.length; i++) {
+        allContent[i].classList.toggle('none');
+      }
+      // allTabs[x-1].classList.add('active');
+    }
+  }
+  vm.changeTab = changeTab;
 
 }
 
