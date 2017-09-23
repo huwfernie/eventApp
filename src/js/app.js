10ffd5b28bdefb662e0bf5daca14e8a26828540c
Huw Fernie <HuwFernie@gmail.com>
2017-09-23 16:02:58 +0100

    back to where we started this morning

--- src/js/app.js
+++ src/js/app.js
@@ -2,17 +2,3 @@ angular
   .module('eventApp', [
     'ui.router', 'ngResource', 'satellizer'
   ]);
-
-// angular
-//   .module('introToAngular', [])
-//   .controller('HomeCtrl', HomeCtrl);
-//
-// // don't use $scope on the course
-// // function HomeCtrl($scope) {
-// //   $scope.message = 'Hey you guys!';
-// // }
-//
-// function HomeCtrl() {
-//   const vm = this;
-//   vm.message = 'Hey you guys!';
-// }
